8b36e61ff40542c7cbcac5f4b86cba8bab57e96b
zxf1231 <zxf1231@hotmail.com>
2017-02-08 21:36:02 +0800

    six

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Overtrue\Wechat\Server;
+use EasyWeChat\Foundation\Application;
 
 class WxController extends Controller
 {
@@ -11,9 +11,21 @@ class WxController extends Controller
 
     public function index()
     {
-        define('TOKEN','php_zxf');
-        $server=new Server(env('WX_ID'),env('WX_TOKEN'));
-        return $server->server();
+        $options = [
+            'debug'     => true,
+            'app_id'    => 'wxa83770b393b6a056',
+            'secret'    => 'f5dfaf9af9184351631029ae3bfb2db5',
+            'token'     => 'php_zxf',
+            'log' => [
+                'level' => 'debug',
+                'file'  => '/tmp/easywechat.log',
+            ],
+            // ...
+        ];
+
+        $app = new Application($options);
+
+        $server = $app->server;
 
     }
 }
