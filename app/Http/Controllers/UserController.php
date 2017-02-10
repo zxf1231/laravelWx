1cb67932d2603a84470bb8d3b0fb6ed3c319eac9
zxf1231 <zxf1231@hotmail.com>
2017-02-10 19:10:19 +0800

    5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,9 +10,21 @@ class UserController extends Controller
     //
     public function login(Request $request)
     {
+        if($request->session()->get('user')){
+            return '/';
+        }
         $auth=new Auth(env('WX_ID'),env('WX_SEC'));
         $user=$auth->authorize();
         $request->session()->put('user',$user->all());
         return back();
+    }
+
+    public function index(Request $request)
+    {
+        if(!$request->session()->get('user')){
+            return url('/login');
+        }
+        dd($request->session()->get('user'));
+
     }
 }
